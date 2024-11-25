9b8b2ff6e1c9d3bbabf3a1a277f7c5a2286d2ae8
Mirhan <111046193+NathanielConstructive@users.noreply.github.com>
2024-11-25 08:18:36 +0100

    Update helper.py

--- helper.py
+++ helper.py
@@ -1,26 +1,50 @@
 from dataclasses import dataclass
+import datetime
 
+# Globale Liste für To-Do-Items
 items = []
 
 
 @dataclass
 class Item:
     text: str
+    date: datetime.date
     isCompleted: bool = False
 
 
-def add(text):
+def add(text, date_str):
+    """
+    Fügt ein neues To-Do hinzu und sortiert die Liste nach Datum.
+    """
+    # Text verarbeiten (B --> Bbb)
     text = text.replace('b', 'bbb').replace('B', 'Bbb')
-    items.append(Item(text))
+
+    # Datum parsen
+    date = datetime.datetime.strptime(date_str, "%Y-%m-%d").date()
+
+    # Item erstellen und hinzufügen
+    items.append(Item(text, date))
+
+    # Liste nach Datum sortieren
+    items.sort(key=lambda x: x.date)
 
 
 def get_all():
+    """
+    Gibt alle To-Do-Items zurück.
+    """
     return items
 
 
 def get(index):
+    """
+    Gibt ein bestimmtes To-Do anhand seines Indexes zurück.
+    """
     return items[index]
 
 
 def update(index):
+    """
+    Aktualisiert den Status eines To-Do-Items (Completed/Not Completed).
+    """
     items[index].isCompleted = not items[index].isCompleted
